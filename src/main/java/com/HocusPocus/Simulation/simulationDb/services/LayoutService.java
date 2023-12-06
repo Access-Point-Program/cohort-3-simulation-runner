22e6fcbb2354f70e4d532452041389e96124fd56
calisward <calisward2005@gmail.com>
2023-12-06 09:23:55 -0600

    Layout service/controller update

--- src/main/java/com/HocusPocus/Simulation/simulationDb/services/LayoutService.java
+++ src/main/java/com/HocusPocus/Simulation/simulationDb/services/LayoutService.java
@@ -1,8 +1,9 @@
 package com.HocusPocus.Simulation.simulationDb.services;
 
 import com.HocusPocus.Simulation.simulationDb.SimulationResults;
+import com.HocusPocus.Simulation.simulationDb.controller.LayoutController;
 import com.HocusPocus.Simulation.simulationDb.models.Layout;
-import com.HocusPocus.Simulation.simulationDb.repo.LayoutsRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,6 @@ import java.util.Optional;
 @Service
 public class LayoutService {
     @Autowired
-    private LayoutsRepo layoutsRepo;
     private WebClient webClient;
 
     public List<Layout> getAllLayouts() {
@@ -26,6 +26,6 @@ public class LayoutService {
     }
 
     public Optional<Layout> getLayoutsById(Long id) {
-        return layoutsRepo.findById(id);
+        return getLayoutsById(id);
     }
 }
