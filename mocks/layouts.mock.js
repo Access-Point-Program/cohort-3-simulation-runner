1dcb72decc36c2c5ecd982b08b16e5d5c0f28079
calisward <calisward2005@gmail.com>
2023-12-06 14:49:48 -0600

    Layout and ruleset getbyid

--- mocks/layouts.mock.js
+++ mocks/layouts.mock.js
@@ -2,18 +2,10 @@ let mockLayouts = require("./data/layouts.json");
 
 
 const proxy = {
-  _proxy: {
-    changeHost: true,
-    httpProxy: {
-      options: {
-        ignorePath: true,
-      },
-    },
-  },
-
   "GET /layouts": (_, res) => {
     res.json(mockLayouts);
   },
+  
   "GET /layouts/:id": (req, res) => {
     mockLayouts = mockLayouts.filter(({ id }) => id != req.params.id);
     res.send();
