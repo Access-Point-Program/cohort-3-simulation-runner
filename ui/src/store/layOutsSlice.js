120a1cb304406089bcad8edee3bee8e8d4baa89e
blenaneraio <138702680+blenaneraio@users.noreply.github.com>
2023-12-06 09:28:10 -0600

    done

--- ui/src/store/layOutsSlice.js
+++ ui/src/store/layOutsSlice.js
@@ -2,22 +2,23 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // IMPORTANT: Change this to 9010 when you have it working in your API.
 // For me, I implement this using the mocks
-const port = 8080; 
+const port = 9003; 
 
 // The example is using the Pokemon API
 // https://redux-toolkit.js.org/tutorials/rtk-query
-export const rulesetsApi = createApi({
-  reducerPath: 'rulesetsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:${port}/`,
-  }),
-  
+export const layoutsApi = createApi({
+    reducerPath: 'layoutsApi',
+    baseQuery: fetchBaseQuery({
+      baseUrl: `http://localhost:${port}/`,
+    }),
+
+    
   // Tags are important for caching to prevent additional API calls.
-  tagTypes: ['RuleSet'],
+  tagTypes: ['Layout'],
   endpoints: (builder) => ({
-    getRuleSets: builder.query({ query: () => `/rulesets` }),
+    getLayouts: builder.query({ query: () => `/layouts` }),
   }),
 })
 
 // Question: how does this function get named?
-export const { useGetRuleSetsQuery } = rulesetsApi
+export const { useGetLayOutsQuery } = layoutsApi
