7a83d1a060ed418625c3c1882d3fe49f65e7118b
Christopher Marfia <chris.marfia@daugherty.com>
2023-12-08 10:12:58 -0600

    Update the rules slice to use the correct URL

--- ui/src/store/ruleSetsSlice.js
+++ ui/src/store/ruleSetsSlice.js
@@ -1,20 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// IMPORTANT: Change this to 9010 when you have it working in your API.
-// For me, I implement this using the mocks
-const port = 9010;
-
 export const rulesetsApi = createApi({
   reducerPath: 'rulesetsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:${port}/`,
+    baseUrl: `/`,
   }),
 
   // Tags are important for caching to prevent additional API calls.
   tagTypes: ['RuleSet'],
   endpoints: (builder) => ({
-    getRuleSets: builder.query({ query: () => `/rulesets` }),
-    getRuleSetById: builder.query({ query: (id) => `/rulesets/${id}` }), // New query endpoint
+    getRuleSets: builder.query({ query: () => `/ruleset` }),
+    getRuleSetById: builder.query({ query: (id) => `/ruleset/${id}` }), // New query endpoint
   }),
 });
 // Question: how does this function get named?
