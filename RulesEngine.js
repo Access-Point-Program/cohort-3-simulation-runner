6176c1df83a7f4b204fbcc299112da803b2ef37d
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2023-12-18 22:02:25 -0600

    okay

---
+++ RulesEngine.js
@@ -0,0 +1,17 @@
+const { Engine } = require("json-rules-engine");
+const rulesets = require ("./mocks/rulesets.mock")
+let engine = Engine();
+
+
+async function start(){
+
+
+const engine = new Engine
+
+
+engine.setCondition ('')
+
+
+
+
+}
