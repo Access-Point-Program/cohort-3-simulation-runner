6d682577781c8b1536a5bd383139eef562b7f9ad
Christopher Marfia <chris.marfia@daugherty.com>
2023-12-20 07:57:31 -0600

    Merge the Simulations.js with the App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,8 @@
-import "./App.css";
-import React, { useState } from "react";
-import Grid from "../Components/grid";
-import Legend from "../Components/Legend";
-import Settings from "../Components/settings";
-import "./Simulation.css";
 import React from "react";
 import SideBar from "./Components/Sidebar";
-import Simulation from "./pages/Simulation";
+import Grid from "../Components/grid";
+import Legend from "../Components/Legend";
+import Settings from "./Components/Settings"
 import "./App.css"; // Import your CSS file for styling
 
 function App() {
@@ -14,44 +10,17 @@ function App() {
     <div className="app-container">
       <SideBar />
       <div className="content-container">
-        <Simulation />
+        <div className="content-container">
+          <Legend />
+          <Grid data={{}} />
+        </div>
+
+        <div className="settings-container">
+          <Settings />
+        </div>
       </div>
     </div>
   );
 }
-
-function Simulation() {
-  const [selectedLayout, setSelectedLayout] = useState(null);
-  const [selectedRuleset, setSelectedRuleset] = useState(null);
-
-  const handleLayoutChange = (layout) => {
-    setSelectedLayout(layout);
-  };
-
-  const handleRulesetChange = (ruleset) => {
-    setSelectedRuleset(ruleset);
-  };
-
-  return (
-    <div>
-      <h1>Simulation Page</h1>
-
-      <div className="content-container">
-        <Legend />
-        <Grid data={selectedLayout} />
-      </div>
-
-      <div className="settings-container">
-        <Settings
-          layout={{}}
-          onSelect={handleLayoutChange}
-          ruleset={{}}
-        />
-
-      </div>
-    </div>
-  );
-}
-
 
 export default App;
