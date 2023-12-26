34952c9b72aaf0dd86317b8cfafd192c97618d2f
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2023-12-26 09:19:48 -0600

    Update Simulation.js

--- ui/src/pages/Simulation.js
+++ ui/src/pages/Simulation.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Grid from "../Components/grid";
 import Legend from "../Components/Legend";
-import Settings from "../Components/settings";
+import Settings from "../Components/Settings";
 // Fix settings
 import "./Simulation.css";
 
