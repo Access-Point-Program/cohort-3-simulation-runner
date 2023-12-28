5b6239d95bae24c60a1229e3af87cfee23010b6a
kwhitehead23 <108090755+kwhitehead23@users.noreply.github.com>
2023-12-28 11:10:14 -0600

    TODO slice tasks

--- ui/src/store/ruleSetsSlice.js
+++ ui/src/store/ruleSetsSlice.js
@@ -1,16 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // TODO - make this no longer call the mocks
-
 export const rulesetsApi = createApi({
   reducerPath: 'rulesetsApi',
-  //baseQuery: fetchBaseQuery({ baseUrl: `/` }),
-  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:9004/` }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9004/' }),
 
   // Tags are important for caching to prevent additional API calls.
   tagTypes: ['RuleSet'],
   endpoints: (builder) => ({
-    getRuleSets: builder.query({ query: () => `/ruleset` }),
+    getRuleSets: builder.query({ query: () => '/ruleset' }),
     getRuleSetById: builder.query({ query: (id) => `/ruleset/${id}` }), // New query endpoint
   }),
 });
