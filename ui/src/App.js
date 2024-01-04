88dddf1c346d115ec39eaa3d8e63e1ee1d527bae
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2024-01-04 10:20:58 -0600

    late work is best work

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,9 +4,12 @@ import Grid from "./Components/grid";
 import Legend from "./Components/Legend";
 import Settings from "./Components/Settings";
 import MoveList from "./Components/moveList";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
 import "./App.css"; // Import your CSS file for styling
 
 function App() {
+  const succeeded = useSelector((state) => state.simulation.succeeded);
+  
   return (
     <div className="app-container">
       <SideBar />
